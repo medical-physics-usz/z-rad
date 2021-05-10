fb2f10ec0de370fc658911506ddaf5d7c2e445ab
martabogowicz <marta.bogowicz@gmail.com>
2021-05-10 21:04:38 +0200

    new class for normalization

--- zrad/main_texture_mr.py
+++ zrad/main_texture_mr.py
@@ -12,12 +12,7 @@ from export import Export
 from read import ReadImageStructure
 from structure import Structures
 from texture import Texture
-
-#normalization libraries
-import nibabel as nib
-from HD_BET.run import run_hd_bet
-import pylab as plt
-
+from normalization import Normalization
 
 class main_texture_mr(object):
     """
@@ -54,12 +49,12 @@ class main_texture_mr(object):
         image_modality = ['MR']
         dicomProblem = []
         MR_UID = ['1.2.840.10008.5.1.4.1.1.4']  # MR
-        meanWV = False #caluclated modified WV transform
 
         def parfor(ImName):
             self.logger.info("Patient " + ImName)
             mypath_image = path_image + ImName + os.sep
             to_return_3d = list()
+            meanWV = False #caluclated modified WV transform
 
             for structure in structures:
                 try:
@@ -68,7 +63,7 @@ class main_texture_mr(object):
 
                     # MR intensities normalization
                     if struct_norm1 != '':
-                        norm_slope, norm_inter = self.normalization_linear(struct_norm1, struct_norm2, read, mypath_image)
+                        norm_slope, norm_inter = Normalization().normalization_linear(struct_norm1, struct_norm2, mypath_image, read.rs, read.slices, read.x_ct, read.y_ct, read.xCTspace, read.onlyfiles, read.rows, read.columns)
                     else:
                         norm_slope = 1  # to allow for calculation with no normalization
                         norm_inter = 0
@@ -98,21 +93,21 @@ class main_texture_mr(object):
                         #normalization ROI
                         if normROI_advanced == 'brain':
                             patPos = dc.read_file(mypath_image + f).PatientPosition
-                            brain_mask = self.maskBrain(patPos, read.xCTspace, read.x_ct, read.y_ct, np.min(read.slices), IM_matrix, path_skull, ImName, path_save)
+                            brain_mask = Normalization().mask_brain(patPos, read.xCTspace, read.x_ct, read.y_ct, np.min(read.slices), IM_matrix, path_skull, ImName, path_save)
                             IM_matrix_masked = IM_matrix * brain_mask  
                         elif normROI_advanced == 'ROI':
                             structROI = Structures(read.rs, [read.structure_f], read.slices, read.x_ct, read.y_ct, read.xCTspace, len(read.slices), False, None, False) #False for the wavelet and local  argument, none for dim
                             norm_Xcontour = structROI.Xcontour
                             norm_Ycontour = structROI.Ycontour
-                            ROI_mask = self.maskROI(norm_Xcontour, norm_Ycontour, IM_matrix.shape, np.nan)
+                            ROI_mask = Normalization().mask_ROI(norm_Xcontour, norm_Ycontour, IM_matrix.shape, np.nan)
                             IM_matrix_masked = IM_matrix * ROI_mask                            
                         elif normROI_advanced == 'brain-ROI':
                             patPos = dc.read_file(mypath_image + f).PatientPosition
-                            brain_mask = self.maskBrain(patPos, read.xCTspace, read.x_ct, read.y_ct, np.min(read.slices), IM_matrix, path_skull, ImName, path_save)
+                            brain_mask = Normalization().mask_brain(patPos, read.xCTspace, read.x_ct, read.y_ct, np.min(read.slices), IM_matrix, path_skull, ImName, path_save)
                             structROI = Structures(read.rs, [read.structure_f], read.slices, read.x_ct, read.y_ct, read.xCTspace, len(read.slices), False, None, False) #False for the wavelet and local  argument, none for dim
                             norm_Xcontour = structROI.Xcontour
                             norm_Ycontour = structROI.Ycontour
-                            ROI_mask = self.maskROI(norm_Xcontour, norm_Ycontour, IM_matrix.shape, 0)
+                            ROI_mask = Normalization().mask_ROI(norm_Xcontour, norm_Ycontour, IM_matrix.shape, 0)
                             ROI_mask += 1
                             ROI_mask[np.where(ROI_mask==2)] = np.nan
                             brainMinusROI_mask = brain_mask * ROI_mask
@@ -124,10 +119,10 @@ class main_texture_mr(object):
                         
                         #normaliztion technique
                         if norm_type == 'histogram matching':
-                            IM_matrix = self.HistMatching(IM_matrix_masked, ImName, path_save, path_template)   
+                            IM_matrix = Normalization().hist_matching(IM_matrix_masked, ImName, path_save, path_template)   
                             meanWV = True #caluclated modified WV transform
                         elif norm_type == 'z-score':
-                            mean, std = self.ZscoreNorm(IM_matrix_masked)      
+                            mean, std = Normalization().zscore_norm(IM_matrix_masked)      
                             IM_matrix = (IM_matrix - mean)/std
 
 
@@ -142,7 +137,7 @@ class main_texture_mr(object):
                 if dim == '3D':
                     lista_results = Texture([IM_matrix], read.structure_f, read.columns, read.rows, read.xCTspace,
                                             read.slices, path_save, ImName, pixNr, binSize, image_modality, wv, local,
-                                            cropStructure, read.stop_calc, read.Xcontour, read.Xcontour_W, read.Ycontour,
+                                            cropStructure, read.stop_calc, meanWV, read.Xcontour, read.Xcontour_W, read.Ycontour,
                                             read.Ycontour_W, read.Xcontour_Rec, read.Ycontour_Rec).ret()
 
                 #                elif dim == '2D': #not working
@@ -162,268 +157,4 @@ class main_texture_mr(object):
             final_file = Export().ExportResults(feature_vec, final_file, par_names, image_modality,
                                                 wave_names, wv, local)
         final_file.close()
-
-    def normalization_linear(self, struct_norm1, struct_norm2, read, mypath_image):
-        """Get the normalization coefficients for MR image based on two normal structures (for example muscle and white
-        matter) and linear function.
-        """
-        # False for the wavelet and local argument, none for dim
-        struct1 = Structures(read.rs, [struct_norm1], read.slices, read.x_ct, read.y_ct,
-                             read.xCTspace, len(read.slices), False, None, False)
-        norm1_Xcontour = struct1.Xcontour
-        norm1_Ycontour = struct1.Ycontour
-
-        struct2 = Structures(read.rs, [struct_norm2], read.slices, read.x_ct, read.y_ct, read.xCTspace,
-                             len(read.slices), False, None,
-                             False)  # False for the wavelet and local  argument, none for dim
-        norm2_Xcontour = struct2.Xcontour
-        norm2_Ycontour = struct2.Ycontour
-
-        # to read only slices where there is a contour
-        ind = []
-        for f in range(len(norm1_Xcontour)):
-            if norm1_Xcontour[f] != []:
-                ind.append(f)
-        for f in range(len(norm2_Xcontour)):
-            if norm2_Xcontour[f] != []:
-                ind.append(f)
-
-        zmin = np.min(ind)
-        zmax = np.max(ind)
-
-        onlyfiles = read.onlyfiles[zmin:zmax + 1]
-        slices = read.slices[zmin:zmax + 1]
-        norm1_Xcontour = norm1_Xcontour[zmin:zmax + 1]
-        norm1_Ycontour = norm1_Ycontour[zmin:zmax + 1]
-        norm2_Xcontour = norm2_Xcontour[zmin:zmax + 1]
-        norm2_Ycontour = norm2_Ycontour[zmin:zmax + 1]
-
-        IM_matrix = []  # list containing the images matrix
-        for i in onlyfiles:
-            data = dc.read_file(mypath_image + i).PixelData
-            data16 = np.array(np.fromstring(data, dtype=np.int16))  # converting to decimal
-            # recalculating for rows x columns
-            a = []
-            for j in range(read.rows):
-                a.append(data16[j * read.columns:(j + 1) * read.columns])
-            a = np.array(a)
-            IM_matrix.append(np.array(a))
-
-        IM_matrix = np.array(IM_matrix)
-
-        v1 = []  # values for structure 1
-        v2 = []  # values for structure 2
-
-        for i in range(len(norm1_Xcontour)):  # slices
-            for j in range(len(norm1_Xcontour[i])):  # sub-structures in the slice
-                for k in range(len(norm1_Xcontour[i][j])):
-                    v1.append(IM_matrix[i][norm1_Ycontour[i][j][k]][norm1_Xcontour[i][j][k]])
-
-        for i in range(len(norm2_Xcontour)):  # slices
-            for j in range(len(norm2_Xcontour[i])):  # sub-structures in the slice
-                for k in range(len(norm2_Xcontour[i][j])):
-                    v2.append(IM_matrix[i][norm2_Ycontour[i][j][k]][norm2_Xcontour[i][j][k]])
-
-        f1 = np.mean(v1)
-        f2 = np.mean(v2)
-
-        fa = (800 - 300) / (f1 - f2)  # find coefficients of a linear function
-        fb = 800 - f1 * fa
-
-        return fa, fb
-    
-    def maskROI(self, norm_Xcontour, norm_Ycontour, matrix_shape, outer_value):
-        mask_data = np.zeros(matrix_shape)  # creating the matrix to fill it with points of the structure, y rows, x columns
-        mask_data[:, :, :] = outer_value
-        for i in range(len(norm_Xcontour)): # slices
-            for j in range(len(norm_Xcontour[i])): #sub-structres in the slice
-                for k in range(len(norm_Xcontour[i][j])):
-                    mask_data[i][norm_Ycontour[i][j][k]][norm_Xcontour[i][j][k]] = 1
-        return mask_data
-    
-    def maskBrain(self, patPos,xCTspace, x_ct, y_ct, z_ct, IM_matrix, path_skull, ImName, path_save):
-        if path_skull == '':
-            if patPos == 'HFS':
-                affine_trans = np.zeros((4,4))
-                affine_trans[0, 0] = -1 * xCTspace
-                affine_trans[1, 1] = -1 * xCTspace
-                affine_trans[2, 2] = xCTspace
-                affine_trans[3, 3] = 1.
-                affine_trans[0, 3] = -1 * x_ct
-                affine_trans[1, 3] = -1 * y_ct
-                affine_trans[2, 3] = z_ct
-                
-                mask_data = self.SkullStripping(IM_matrix, affine_trans, path_save, ImName)
-            else:
-                self.logger = logging.getLogger("MR read")
-                self.logger.info("Scan position not HFS, the skull stripping was not tested")
-                raise TypeError
-        elif path_skull != '':
-            try:
-                mask = nib.load(path_skull + os.sep + str(ImName) + '_mask.nii.gz')
-            except WindowsError:
-                mask = nib.load(path_skull + os.sep + str(ImName) + '_mask.nii')
-            mask_data = mask.get_fdata()
-
-            mask_data[np.where(mask_data == 0)] = np.nan
-            
-        return mask_data
-    
-    def SkullStripping(self, IM_matrix, transform, path_save, name):
-        IM_matrix_nifti = nib.Nifti1Image(IM_matrix, affine = transform)
-        try:
-            makedirs(path_save + os.sep+ 'nifti' + os.sep)
-        except OSError:
-            if not isdir(path_save + os.sep + 'nifti' + os.sep):
-                raise
-        nib.save(IM_matrix_nifti, path_save + os.sep + 'nifti' + os.sep + str(name) + '.nii.gz')  
-        del IM_matrix_nifti
-        
-        input_files = [path_save + os.sep + 'nifti' + os.sep + str(name) + '.nii.gz']
-        output_files = [path_save + os.sep + 'brain_mask' + os.sep + str(name) + '.nii.gz']
-        
-        try:
-            makedirs(path_save + os.sep + 'brain_mask' + os.sep)
-        except OSError:
-            if not isdir(path_save + os.sep + 'brain_mask' + os.sep):
-                raise
-        
-        #settings as from https://github.com/MIC-DKFZ/HD-BET/blob/master/HD_BET/hd-bet
-        mode= 'fast' #fast\' or \'accurate
-        config_file= 'config_hd_bet.py' #D:\\radiomics\\SPHN\\normalization\\HD-BET-master\\HD_BET\\config.py'
-        device= 'cpu'
-        pp= True
-        tta=  True
-        save_mask= True 
-        overwrite_existing = False
-        
-        run_hd_bet(input_files, output_files, mode, config_file, device, pp, tta, save_mask, overwrite_existing)
-        
-        try:
-            mask = nib.load(path_save + os.sep + 'brain_mask' + os.sep + str(name) + '_mask.nii.gz')
-        except WindowsError:
-            mask = nib.load(path_save + os.sep + 'brain_mask' + os.sep + str(name) + '_mask.nii')
-        mask_data = mask.get_fdata()
-        
-        mask_data[np.where(mask_data == 0)] = np.nan
-        
-        a = IM_matrix * mask_data
-        ###test segmentation
-        for n in range(len(a)//8+1):
-            fig = plt.figure(10, figsize=(20, 20))
-            fig.text(0.5, 0.95, name)
-            for j in range(8):
-                axes = fig.add_subplot(3,3, j+1)
-                axes.set_title(8*n+j)
-                try:
-                    im = axes.imshow(a[8 * n + j], cmap=plt.get_cmap('jet'), vmin=0, vmax=np.nanmax(a))
-                except IndexError:
-                    break
-                    pass
-            axes = fig.add_subplot(3, 3, 9)
-            try:
-                fig.colorbar(im)
-            except UnboundLocalError:
-                pass
-            try:
-                makedirs(path_save+ os.sep +'brain_mask_png' +os.sep+name+os.sep)
-            except OSError:
-                if not isdir(path_save+os.sep+'brain_mask_png'+os.sep+name+os.sep):
-                    raise
-            fig.savefig(path_save+os.sep+'brain_mask_png'+os.sep+name+os.sep+str(n+1)+'.png')
-            plt.close()
-        
-        return mask_data
-
-    def HistMatching(self, newBrain, name, path_save, path_template):
-        #matched = match_histograms(newBrain, standardBrain, multichannel=False)
-        """
-        Return modified source array so that the cumulative density function of
-        its values matches the cumulative density function of the template.
-        """
-        src_values, src_unique_indices, src_counts = np.unique(newBrain.ravel(), return_inverse=True, return_counts=True)
-        #src_values - unique values
-        #src_unique_indices - indices per voxel to given unique value, eash nan voxel is considered a unique value and has to be handeled separately
-        #src_counts - counts how many time thie unique value occurs
-#        example 
-#        a = array([[nan,  2.,  3.,  4.,  4.],
-#        [ 4.,  4.,  4.,  5., nan]])
-#        np.unique(a.ravel(), return_inverse=True, return_counts=True)
-#        (array([ 2.,  3.,  4.,  5., nan, nan]),
-#         array([4, 0, 1, 2, 2, 2, 2, 2, 3, 5], dtype=int64),
-#         array([1, 1, 5, 1, 1, 1], dtype=int64))
-        
-        #read template
-        standardBrain = np.load(path_template)
-
-        tmpl_values, tmpl_counts = np.unique(standardBrain.ravel(), return_counts=True)
-        
-        #check for nan
-        src_nan = np.min(np.where(np.isnan(src_values)))
-        src_counts = src_counts[ : src_nan]
-        
-        
-        tmpl_nan = np.min(np.where(np.isnan(tmpl_values)))
-        tmpl_counts = tmpl_counts[ : tmpl_nan]
-        tmpl_values = tmpl_values[ : tmpl_nan]
-    
-        # calculate normalized quantiles for each array
-        src_quantiles = np.cumsum(src_counts) / (newBrain.size - len(np.where(np.isnan(newBrain))[0]))
-        tmpl_quantiles = np.cumsum(tmpl_counts) / (standardBrain.size - len(np.where(np.isnan(standardBrain))[0]))
-    
-        interp_a_values = np.interp(src_quantiles, tmpl_quantiles, tmpl_values)
-        
-        src_new_values = np.zeros(len(src_unique_indices))
-        src_new_values[ : ] = np.nan 
-        for i, sind in enumerate(src_unique_indices):
-            if sind < src_nan:
-                src_new_values[i] = interp_a_values[sind]
-                
-        a=src_new_values.reshape(newBrain.shape)
-        ###test segmentation
-        for n in range(len(a)//8+1):
-            fig = plt.figure(10, figsize=(20, 20))
-            fig.text(0.5, 0.95, name)
-            for j in range(8):
-                axes = fig.add_subplot(3,3, j+1)
-                axes.set_title(8*n+j)
-                try:
-                    im = axes.imshow(a[8 * n + j], cmap=plt.get_cmap('jet'))
-                except IndexError:
-                    break
-                    pass
-            axes = fig.add_subplot(3, 3, 9)
-            try:
-                fig.colorbar(im)
-            except UnboundLocalError:
-                pass
-            try:
-                makedirs(path_save+os.sep+'brain_mask_hist'+os.sep+name+os.sep)
-            except OSError:
-                if not isdir(path_save+os.sep+'brain_mask_hist'+os.sep+name+os.sep):
-                    raise
-            fig.savefig(path_save+os.sep+'brain_mask_hist'+os.sep+name+os.sep+str(n+1)+'.png')
-            plt.close()
-            
-        affine_trans = np.zeros((4,4))
-        affine_trans[0, 0] = -1 * 0.5
-        affine_trans[1, 1] = -1 * 0.5
-        affine_trans[2, 2] = 0.5
-        affine_trans[3, 3] = 1.
-        affine_trans[0, 3] = -1 * 100
-        affine_trans[1, 3] = -1 * 100
-        affine_trans[2, 3] = 100
-        
-        IM_matrix_nifti = nib.Nifti1Image(a, affine = affine_trans)
-        nib.save(IM_matrix_nifti, path_save+os.sep+'brain_mask_hist'+os.sep+name+os.sep+'norm.nii.gz')        
-             
-        return src_new_values.reshape(newBrain.shape)
-    
-    def ZscoreNorm(self, ROI):
-        mean = np.nanmean(ROI)
-        std = np.nanstd(ROI)
-        
-        return mean, std
-    
-
 

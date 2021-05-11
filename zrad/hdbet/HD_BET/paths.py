37fa2da61ebf1a9c97ad7e7b7284511386c652bf
martabogowicz <marta.bogowicz@gmail.com>
2021-05-11 17:54:39 +0200

    add the HD-BET code

---
+++ zrad/hdbet/HD_BET/paths.py
@@ -0,0 +1,5 @@
+import os
+
+# please refer to the readme on where to get the parameters. Save them in this folder:
+#folder_with_parameter_files = os.path.join('D:\\radiomics\\SPHN\\normalization\\HD-BET-master\\HD_BET\\', 'hd-bet_params')
+folder_with_parameter_files = os.path.join('hdbet' + os.sep + 'HD_BET', 'hd-bet_params')

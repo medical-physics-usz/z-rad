db0d3c602acdd5e8153a17417e2c916c745af1ec
Hubert Gabryś <hubert.gabrys@gmail.com>
2021-08-25 20:20:30 +0200

    Created a function for generating an executable.

---
+++ zrad/generate_executable.py
@@ -0,0 +1,11 @@
+import PyInstaller.__main__
+
+PyInstaller.__main__.run([
+    'main_texture.py',
+    '--onefile',
+    '--add-data=config.txt;.',
+    '--add-data=LogoUSZ.png;.',
+    '--add-data=feature_names_2D.txt;.',
+    '--hidden-import=vtkmodules',
+    '--hidden-import=vtkmodules.all',
+])

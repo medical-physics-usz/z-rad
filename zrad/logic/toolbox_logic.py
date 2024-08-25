d4d4330c2a1d90145a7e1c8684a11a984d887178
Hubert Gabrys <hubert.gabrys@gmail.com>
2024-08-25 15:51:58 +0200

    Added new functions for downloading files from URLs and GitHub repositories.

--- zrad/logic/toolbox_logic.py
+++ zrad/logic/toolbox_logic.py
@@ -1,6 +1,9 @@
+import json
 import logging
 import os
 import sys
+import time
+import urllib.request
 from datetime import datetime
 
 import SimpleITK as sitk
@@ -540,3 +543,96 @@ def close_all_loggers():
         for handler in logger.handlers[:]:
             handler.close()
             logger.removeHandler(handler)
+
+
+def download_file(url, local_filename, verbose=False):
+    """
+    Downloads a file from the specified URL and saves it to the local file system.
+
+    Args:
+        url (str): The URL of the file to download.
+        local_filename (str): The local path where the file should be saved.
+        verbose (bool): If True, prints download status messages.
+
+    Raises:
+        URLError: If there's an issue with the network or URL.
+        IOError: If there's an issue writing the file to the local system.
+    """
+    try:
+        with urllib.request.urlopen(url) as response:
+            with open(local_filename, 'wb') as out_file:
+                out_file.write(response.read())
+        if verbose:
+            print(f"Downloaded {local_filename}")
+    except urllib.error.URLError as e:
+        print(f"Failed to download {url}. Error: {e}")
+        raise
+    except IOError as e:
+        print(f"Failed to save {local_filename}. Error: {e}")
+        raise
+
+
+def fetch_github_directory_files(owner, repo, directory_path, save_path=None, token=None, verbose=False):
+    """
+    Fetches all files from a specified GitHub repository directory and saves them locally.
+
+    Args:
+        owner (str): The GitHub username or organization that owns the repository.
+        repo (str): The name of the GitHub repository.
+        directory_path (str): The path to the directory within the repository.
+        save_path (str, optional): The local directory where the downloaded files will be saved.
+        token (str, optional): GitHub personal access token for authentication.
+        verbose (bool, optional): If True, print progress messages.
+
+    Raises:
+        ValueError: If an unsupported imaging format or chapter is specified.
+        URLError: If there's an issue with the network or GitHub API.
+        IOError: If there's an issue writing files to the local system.
+    """
+    api_url = f"https://api.github.com/repos/{owner}/{repo}/contents/{directory_path}"
+    headers = {}
+    if token:
+        headers['Authorization'] = f'token {token}'
+
+    try:
+        with urllib.request.urlopen(urllib.request.Request(api_url, headers=headers)) as response:
+            items = json.loads(response.read().decode())
+
+            for item in items:
+                item_path = item['path']  # Full path of the item in the repo
+                relative_path = os.path.relpath(item_path,
+                                                directory_path)  # Relative path within the specified directory
+
+                if item['type'] == 'file':
+                    download_url = item['download_url']
+                    if save_path:
+                        local_path = os.path.join(save_path, relative_path)
+                    else:
+                        local_path = os.path.join(directory_path, relative_path)
+
+                    os.makedirs(os.path.dirname(local_path), exist_ok=True)
+                    if verbose:
+                        print(f"Downloading {relative_path}...")
+                    download_file(download_url, local_path, verbose)
+                    if verbose:
+                        print(f"{relative_path} downloaded.")
+                elif item['type'] == 'dir':
+                    # Recursive call to handle subdirectories
+                    new_save_path = os.path.join(save_path, relative_path) if save_path else None
+                    fetch_github_directory_files(owner, repo, item_path, new_save_path, token, verbose)
+
+    except urllib.error.HTTPError as e:
+        if e.code == 403:
+            print("Rate limit exceeded. Waiting for 60 seconds before retrying...")
+            time.sleep(60)  # Wait for 60 seconds before retrying
+            fetch_github_directory_files(owner, repo, directory_path, save_path, token, verbose)
+        else:
+            print(f"Failed to fetch directory contents from {api_url}. Error: {e}")
+            raise
+    except urllib.error.URLError as e:
+        print(f"Failed to fetch directory contents from {api_url}. Error: {e}")
+        raise
+    except IOError as e:
+        print(f"Failed to create directories or save files to {save_path}. Error: {e}")
+        raise
+

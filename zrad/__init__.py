2042797634d4b98ba4b4b0031ce4197a64b4878e
Maksym Fritsak <Maksym.Fritsak@usz.ch>
2024-10-29 16:09:04 +0100

    Refactoring to simplify the API

--- zrad/__init__.py
+++ zrad/__init__.py
@@ -1,1 +1,5 @@
 __version__ = "24.10"
+from .logic import Preprocessing
+from .logic import Filtering
+from .logic import Radiomics
+from .logic import Image
--- zrad/logic/__init__.py
+++ zrad/logic/__init__.py
@@ -1,2 +1,4 @@
 from .preprocessing import Preprocessing
+from .filtering import Filtering
 from .radiomics import Radiomics
+from .image import Image

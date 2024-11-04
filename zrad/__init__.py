2c5204d6c3cbc3998001df1f7f96f2db09589ee3
Maksym Fritsak <Maksym.Fritsak@usz.ch>
2024-11-04 10:33:30 +0100

    Changed main __init__.py file, classes are modular now.

--- zrad/__init__.py
+++ zrad/__init__.py
@@ -1,5 +1,1 @@
-__version__ = "24.11"
-from .preprocessing import Preprocessing
-from .filtering import Filtering
-from .radiomics import Radiomics
-from image import Image
+__version__ = "24.11-dev"

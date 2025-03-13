0a3acb30df319c82b3b816fa2d632bdc8ef88046
elise-robinson <elise.robinson18@gmail.com>
2025-03-13 16:38:31 +0100

    Modified code with Maksym includes RTDOSE

--- zrad/image.py
+++ zrad/image.py
@@ -116,6 +116,41 @@ class Image:
         self.direction = mask.direction
         self.shape = mask.shape
 
+    def read_dicom_dose(self, rtdose_path):
+        dose = self.extract_dicom_dose(rtdose_path)
+        self.array = sitk.GetArrayFromImage(dose)
+        self.origin = dose.GetOrigin()
+        self.spacing = dose.GetSpacing()
+        self.direction = dose.GetDirection()
+        self.shape = dose.GetSize()
+
+    def extract_dicom_dose(self, rtdose_path):
+
+        ds = pydicom.dcmread(rtdose_path)
+
+        if ds.DoseUnits != 'GY':
+            raise TypeError(f"Only dose in Gy is supported. Provided {ds.DoseUnits}")
+        if ds.DoseType != 'PHYSICAL':
+            raise TypeError(f"Only physical dose is supported. Provided {ds.DoseType}")
+        # print(f"Min pixel value before rescaling: {np.min(ds.pixel_array)}") # Checking the dose unit values
+        # print(f"Max pixel value before rescaling: {np.max(ds.pixel_array)}") # Checking the dose unit values
+
+        raw_dose_image = sitk.ReadImage(rtdose_path)
+        dose_array = sitk.GetArrayFromImage(raw_dose_image) * ds.DoseGridScaling
+        # print(f"Min pixel value after rescaling: {np.min(dose_array)} Gray") # Checking the rescaled dose unit values (should be between 0-100)
+        # print(f"Max pixel value after rescaling: {np.max(dose_array)} Gray") # Checking the rescaled dose unit values (should be between 0-100)
+        dose_image = sitk.GetImageFromArray(
+            dose_array)  # dose_array is a NumPy array that contains the dose values (after applying DoseGridScaling). This line converts dose_array back into a SITK image.
+        dose_image.SetOrigin(
+            raw_dose_image.GetOrigin())  # Copies the origin (spatial reference of the image in the coordinate system) from the original DICOM image (raw_dose_image) to dose_image.
+        dose_image.SetSpacing(
+            raw_dose_image.GetSpacing())  # Ensures that the voxel spacing (physical size of each pixel in millimeters) is retained from the original dose DICOM file.
+        dose_image.SetDirection(
+            raw_dose_image.GetDirection())  # Copies the image orientation (how the image is aligned in 3D space) from the original dose DICOM file.
+
+        return dose_image
+
+
 
 def get_dicom_files(directory, modality):
     modality_mapping = {'PET': 'PT', 'CT': 'CT', 'MRI': 'MR', 'RTSTRUCT': 'RTSTRUCT', 'MG': 'MG'}

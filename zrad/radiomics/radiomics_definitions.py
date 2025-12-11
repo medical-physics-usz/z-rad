Add error messages to gradient defs fix

zrad/radiomics/radiomics_definitions.py
@@ -603,31 +603,27 @@ class IntensityBasedStatFeatures:
 
     def calc_max_hist_gradient(self, array):  # 3.4.20
         values, counts = np.unique(array[~np.isnan(array)], return_counts=True)
-        counts_gradient = np.gradient(counts)
-        if len(counts_gradient) <=1:
+        if len(counts) <= 1:
             raise DataStructureError(f"Not enough bins to calculate gradient.")
-        self.max_hist_gradient = np.max(counts_gradient)
+        self.max_hist_gradient = np.max(np.gradient(counts))
 
     def calc_max_hist_gradient_intensity(self, array):  # 3.4.21
         values, counts = np.unique(array[~np.isnan(array)], return_counts=True)
-        counts_gradient = np.gradient(counts)
-        if len(counts_gradient) <= 1:
+        if len(counts) <= 1:
             raise DataStructureError(f"Not enough bins to calculate gradient.")
-        self.max_hist_gradient_intensity = values[np.argmax(counts_gradient)]
+        self.max_hist_gradient_intensity = values[np.argmax(np.gradient(counts))]
 
     def calc_min_hist_gradient(self, array):  # 3.4.22
         values, counts = np.unique(array[~np.isnan(array)], return_counts=True)
-        counts_gradient = np.gradient(counts)
-        if len(counts_gradient) <= 1:
+        if len(counts) <= 1:
             raise DataStructureError(f"Not enough bins to calculate gradient.")
-        self.min_hist_gradient = np.min(counts_gradient)
+        self.min_hist_gradient = np.min(np.gradient(counts))
 
     def calc_min_hist_gradient_intensity(self, array):  # 3.4.23
         values, counts = np.unique(array[~np.isnan(array)], return_counts=True)
-        counts_gradient = np.gradient(counts)
-        if len(counts_gradient) <= 1:
+        if len(counts) <= 1:
             raise DataStructureError(f"Not enough bins to calculate gradient.")
-        self.min_hist_gradient_intensity = values[np.argmin(counts_gradient)]
+        self.min_hist_gradient_intensity = values[np.argmin(np.gradient(counts))]
 
 
 class IntensityVolumeHistogramFeatures: